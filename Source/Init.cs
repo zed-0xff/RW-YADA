9236d0b80dc0a15b66b16fed0f43dafbaa4865eb
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-05-12 23:39:05 +0300

    working XML dynamic patch

--- Source/Init.cs
+++ Source/Init.cs
@@ -1,7 +1,4 @@
 using HarmonyLib;
-using System;
-using System.Reflection;
-using System.Reflection.Emit;
 using Verse;
 
 namespace zed_0xff.YADA;
@@ -9,38 +6,9 @@ namespace zed_0xff.YADA;
 [StaticConstructorOnStartup]
 public class Init
 {
-    static Init()
-    {
+    static Init() {
         Harmony harmony = new Harmony("zed_0xff.YADA");
         harmony.PatchAll();
-
-        foreach( var x in DefDatabase<PatchDef>.AllDefsListForReading ){
-            Log.Warning("[d] " + x);
-        }
-
-        // TODO: proper names
-        var assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("ass"), AssemblyBuilderAccess.Run);
-        var module = assembly.DefineDynamicModule("mod");
-        var tb = module.DefineType("t0", TypeAttributes.Public | TypeAttributes.UnicodeClass);
-
-        // ctor of a [HarmonyPatch()] attribute
-        ConstructorInfo attrCtor = typeof(HarmonyPatch).GetConstructor( new Type[]{typeof(string), typeof(string), typeof(MethodType)} );
-        CustomAttributeBuilder cab = new CustomAttributeBuilder( attrCtor, new object[] {"Need", "get_CurLevel", MethodType.Normal});
-        tb.SetCustomAttribute(cab);
-
-        MethodBuilder mb = tb.DefineMethod("Postfix",
-                    MethodAttributes.Public | MethodAttributes.Static,
-                    typeof(float),
-                    new Type[] { typeof(float) });
-
-        mb.DefineParameter(1, ParameterAttributes.None, "__result");
-
-        ILGenerator il = mb.GetILGenerator();
-        il.Emit(OpCodes.Ldc_R4, 0.2f);
-        il.Emit(OpCodes.Ret);
-
-        tb.CreateType();
-
-        harmony.PatchAll(assembly);
+        PatchDynamic.PatchAll();
     }
 }
--- Source/PatchDef.cs
+++ Source/PatchDef.cs
@@ -1,18 +1,91 @@
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Verse;
 
 namespace zed_0xff.YADA;
 
-// Field 'PatchDef.className' is never assigned to, and will always have its default value null
-#pragma warning disable CS0649
+// Field 'PatchDef.className' is never assigned to / never used
+#pragma warning disable CS0649, CS0169
 
 public class PatchDef : Def {
 
-    string className;
+    public enum PatchType { Undefined, Prefix, Postfix }
+    public enum ActionType { None, LogValue, ReturnConst }
+
+    public class Prefix {
+        public bool skipOriginal;
+        public string setResult;
+
+        [Unsaved(false)]
+        public object setResultObj;
+    }
+
+    public class Postfix {
+        public string setResult;
+
+        [Unsaved(false)]
+        public object setResultObj;
+    }
+
+    public string className;
+    public string methodName;
+
+    public Prefix prefix;
+    public Postfix postfix;
+
+    private static int unnamedIdx = 0;
+
+    [Unsaved(false)]
+    public Type resultType;
 
     public override void PostLoad() {
+        if( defName == "UnnamedDef" ){
+            defName = "YADA_dynamic_patch__" + unnamedIdx++ + "__" + className + "__" + methodName;
+        }
         base.PostLoad();
-        Log.Warning("[d] PatchDef.PostLoad " + this + " " + className);
+    }
+
+    public override IEnumerable<string> ConfigErrors() {
+        foreach (string item in base.ConfigErrors()) {
+            yield return item;
+        }
+        if (className == null) yield return "className should be set";
+        if (methodName == null) yield return "methodName should be set";
+        if (prefix == null && postfix == null) yield return "no prefix nor postfix is set";
+
+        Type t = AccessTools.TypeByName(className);
+        if( t == null ){
+            yield return "can't find class " + className;
+            yield break;
+        }
+        MethodInfo m = AccessTools.Method(t, methodName);
+        if( m == null ){
+            yield return "can't find method " + methodName + " in " + t;
+            yield break;
+        }
+        resultType = m.ReturnType;
+
+        if (prefix != null){
+            if( prefix.setResult != null ){
+                if( !ParseHelper.CanParse(resultType, prefix.setResult) ){
+                    yield return "can't parse \"" + prefix.setResult + "\" as " + resultType;
+                    yield break;
+                }
+                prefix.setResultObj = ConvertHelper.Convert(prefix.setResult, resultType);
+            }
+        }
+        if (postfix != null){
+            if( postfix.setResult != null ){
+                if( !ParseHelper.CanParse(resultType, postfix.setResult) ){
+                    yield return "can't parse \"" + postfix.setResult + "\" as " + resultType;
+                    yield break;
+                }
+                postfix.setResultObj = ConvertHelper.Convert(postfix.setResult, resultType);
+            }
+        }
     }
 }
 
-#pragma warning restore CS0649
+#pragma warning restore CS0649, CS0169
